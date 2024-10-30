e6cd5da4a3410a57a5e797780fe6d7b2fe21add5
BogGoro <151636316+BogGoro@users.noreply.github.com>
2024-10-30 14:32:35 +0300

    Russell's approximation method

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def north_west(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]]:
     i, j = 0, 0
-    allocation = np.zeros((len(S), len(D)))
+    allocation = [[0 for _ in range(len(C))] for _ in range(len(S))]
     while i < len(S) and j < len(D):
         amount = min(S[i], D[j])
-        allocation[i, j] = amount
+        allocation[i][j] = amount
         S[i] -= amount
         D[j] -= amount
 
@@ -19,11 +19,29 @@ def north_west(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]
     return allocation
 
 
-def vogel(S: list[int], C: list[list[int]], D: list[int]) -> list[int]:
+def vogel(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]]:
     pass
 
-def russel(S: list[int], C: list[list[int]], D: list[int]) -> list[int]:
-    pass
+def russel(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]]:
+    allocation = [[0 for _ in range(len(C))] for _ in range(len(S))]
+    max_in_row = [max(row) for row in C]
+    max_in_column = [max([C[i][j] for i in range(len(S))]) for j in range(len(D))]
+    while sum(S) > 0 and sum(D) > 0:
+        table = np.array([
+                    [
+                        (C[i][j] - max_in_row[i] - max_in_column[j])
+                        if D[j] > 0 and S[i] > 0 else 1
+                        for j in range(len(D))
+                    ]
+                    for i in range(len(S))
+                ])
+        i, j = np.unravel_index(np.argmin(table), table.shape)
+        amount = min(S[i], D[j])
+        allocation[i][j] = amount
+        S[i] -= amount
+        D[j] -= amount
+
+    return allocation
 
 
 def main():
