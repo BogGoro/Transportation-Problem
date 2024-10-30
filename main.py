5fff6d8b430b969573365fa955059e9dbcfd917c
BogGoro <151636316+BogGoro@users.noreply.github.com>
2024-10-30 18:28:05 +0300

    Vogel's approximation method:

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import numpy as np
 
 def north_west(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]]:
     i, j = 0, 0
-    allocation = [[0 for _ in range(len(C))] for _ in range(len(S))]
+    allocation = [[0 for _ in range(len(D))] for _ in range(len(S))]
     while i < len(S) and j < len(D):
+        # allocating
         amount = min(S[i], D[j])
         allocation[i][j] = amount
         S[i] -= amount
         D[j] -= amount
 
+        # moving
         if S[i] == 0:
             i += 1
         if D[j] == 0:
@@ -20,22 +22,81 @@ def north_west(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]
 
 
 def vogel(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]]:
-    pass
+    allocation = [[0 for _ in range(len(D))] for _ in range(len(S))]
+    C_inf = max([max(row) for row in C]) + 1
+
+    while sum(S) > 0 and sum(D) > 0:
+        C_transpose = [[C[i][j] for i in range(len(S))] for j in range(len(D))]
+        C_transpose_sorted = [sorted(C_transpose[i]) for i in range(len(D))]
+        C_sorted = [sorted(C[i]) for i in range(len(S))]
+        
+        # calculating differences of two minimums in rows and columns
+        min_diff_in_rows = [
+            C_sorted[i][1] - C_sorted[i][0]
+            if S[i] != 0 else -1
+            for i in range(len(S))
+        ]
+        min_diff_in_columns = [
+            C_transpose_sorted[i][1] - C_transpose_sorted[i][0]
+            if D[i] != 0 else -1
+            for i in range(len(D))
+        ]
+
+        diff_list = [min_diff_in_rows]
+        diff_list.append(min_diff_in_columns)
+        diff_list = np.array(diff_list)
+
+        i, j = np.unravel_index(np.argmax(diff_list), diff_list.shape)
+
+        # fining the basic variable index
+        basic_variable_arr = np.array([
+            C_transpose[j][k] if S[k] != 0 else C_inf
+            for k in range(len(S))
+        ]) if i else np.array([
+            C[j][k] if D[k] != 0 else C_inf
+            for k in range(len(D))
+        ])
+        if i:
+            i = np.unravel_index(np.argmin(basic_variable_arr), basic_variable_arr.shape)
+        else:
+            i = j
+            j = np.unravel_index(np.argmin(basic_variable_arr), basic_variable_arr.shape)[0]
+
+        # allocating
+        amount = min(S[i], D[j])
+        allocation[i][j] = amount
+        S[i] -= amount
+        D[j] -= amount
+        
+        # replacing cells with demand = 0 or supply = 0 with pseudo infinity
+        C = [
+            [
+                C[i][j] if D[j] != 0 and S[i] != 0 else C_inf
+                for j in range(len(D))
+            ]
+            for i in range(len(S))
+        ]
+
+    return allocation
 
 def russel(S: list[int], C: list[list[int]], D: list[int]) -> list[list[int]]:
-    allocation = [[0 for _ in range(len(C))] for _ in range(len(S))]
+    allocation = [[0 for _ in range(len(D))] for _ in range(len(S))]
     max_in_row = [max(row) for row in C]
     max_in_column = [max([C[i][j] for i in range(len(S))]) for j in range(len(D))]
     while sum(S) > 0 and sum(D) > 0:
+        # table of differences with maximums, but if demand or supply is equal to 0 then it is 1
         table = np.array([
-                    [
-                        (C[i][j] - max_in_row[i] - max_in_column[j])
-                        if D[j] > 0 and S[i] > 0 else 1
-                        for j in range(len(D))
-                    ]
-                    for i in range(len(S))
-                ])
+            [
+                C[i][j] - max_in_row[i] - max_in_column[j]
+                if D[j] > 0 and S[i] > 0 else 1
+                for j in range(len(D))
+            ]
+            for i in range(len(S))
+        ])
+
+        # finding the minimum in the table
         i, j = np.unravel_index(np.argmin(table), table.shape)
+        # allocating
         amount = min(S[i], D[j])
         allocation[i][j] = amount
         S[i] -= amount
@@ -56,10 +117,10 @@ def main():
         if (sum(S) != sum(D)):
             print("The problem is not balanced!")
             return
-        print(tabulate(table, headers = 'keys', tablefmt = 'psql'))
         solution1 = north_west(S.copy(), C, D.copy())
-        solution2 = vogel(S.copy(), C, D.copy())
+        solution2 = vogel(S.copy(), C.copy(), D.copy())
         solution3 = russel(S.copy(), C, D.copy())
+        print(tabulate(table, headers = 'keys', tablefmt = 'psql'))
         print("North-West method: ", solution1)
         print("Vogel's approximation method: ", solution2)
         print("Russell's approximation method: ", solution3)
